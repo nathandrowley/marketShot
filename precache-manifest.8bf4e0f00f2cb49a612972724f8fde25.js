fefa4aaa1c603539f9c6404b4fd0c239060ed2a6
Nathan Rowley <nathandrowley@gmail.com>
2020-07-02 15:18:11 -0700

    Updates

---
+++ precache-manifest.8bf4e0f00f2cb49a612972724f8fde25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c14754e596a67d094b4c2981c96b2536",
+    "url": "/marketShot/index.html"
+  },
+  {
+    "revision": "a27b9a0f5b1acd71a3ea",
+    "url": "/marketShot/static/css/main.df960b76.chunk.css"
+  },
+  {
+    "revision": "7e76c15b7e70c13ec721",
+    "url": "/marketShot/static/js/2.02097557.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marketShot/static/js/2.02097557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a27b9a0f5b1acd71a3ea",
+    "url": "/marketShot/static/js/main.b42b60f1.chunk.js"
+  },
+  {
+    "revision": "06d6d758f25ad41a93b6",
+    "url": "/marketShot/static/js/runtime-main.aaf46cc9.js"
+  },
+  {
+    "revision": "e5707cb75d2a8df86739498f2f753d30",
+    "url": "/marketShot/static/media/background.e5707cb7.jpg"
+  }
+]);
