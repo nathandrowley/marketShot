4a1783923767cfdc00e73fd365d15f73f227e8f3
Nathan Rowley <nathandrowley@gmail.com>
2020-07-10 14:33:40 -0700

    final ajdustments

--- marketshotapp/src/App.js
+++ marketshotapp/src/App.js
@@ -5,11 +5,11 @@ import MarketTable from "./MarketTable";
 
 export default function App() {
   const [data, setData] = useState();
-  const [symbol, setSymbol] = useState("TSLA");
+  const [symbol, setSymbol] = useState("");
   
   useEffect(() => {
     const grabData = async () => {
-      const apiResult = await fetch(`https://financialmodelingprep.com/api/v3/company/profile/${symbol}?apikey=0741c5e674225d8d08372ba8d6420d93`);
+      const apiResult = await fetch(`https://financialmodelingprep.com/api/v3/company/profile/${symbol.toUpperCase()}?apikey=demo`);
       setData(await apiResult.json());
     };
     grabData();
@@ -21,18 +21,19 @@ export default function App() {
       <div className="App">
         <br></br>
      <div><img src={require('./MarketShotLogo.png')} alt="Market Shot Logo"/>
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-
-      <h1>Market Shot</h1>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" ,}}>
+       <h2>Search Companies</h2>
+       <h3>Get data, Take your shot</h3>
+     
+      
 
       <input value={symbol} onChange={event => setSymbol(event.target.value)} placeholder="Market Symbol (EG AAPL)" />
         <MarketTable data={data} />
-
       </div>
-      <div>
+      {/* <div>
         <h1>This is the raw data</h1>
         <textarea style={{ minHeight: 300, minWidth: 400 }} value={JSON.stringify(data, null, 2)} />
-      </div>
+      </div> */}
     </div>
     </div>
     </div>
--- marketshotapp/src/MarketTable.js
+++ marketshotapp/src/MarketTable.js
@@ -1,30 +1,36 @@
 import React from "react";
+import classes from './MarketTable.module.css';
 
 export default function MarketTable(props) {
   const { data } = props || {};
   const { profile = {} } = data || {};
 
+  // return (
+  //   <div className={classes.anything}>
+  //     <h2>Company Name{profile.companyName}</h2>
+  //     <h3>Price USD ${profile.price}</h3>
+  //     <h3>{profile.beta}</h3>
+  //     <h3>{profile.volAvg}</h3>
+  //     <h3>{profile.mktCap}</h3>
+
+  //   </div>
+  // );
   return (
-    <table>
+    <table className={classes.table}>
       <tr>
-        <th>Link</th>
-        <th>Price</th>
-        <th>Beta</th>
-        <th>Volume Average</th>
-        <th>Market Cap</th>
-      </tr>
-      <tr>
+        <th>Company</th>
         <td>
           <a style={{ display: "flex", flexDirection: "row", alignItems: "center" }} href={profile.website}>
-            <img style={{ height: 20 }} src={profile.image} alt={"company logo"} />
+            <img style={{ height: 20, marginRight: 8}} src={profile.image} alt={"company logo"} />
             {profile.companyName}
           </a>
         </td>
-        <td>{profile.price}</td>
-        <td>{profile.beta}</td>
-        <td>{profile.volAvg}</td>
-        <td>{profile.mktCap}</td>
       </tr>
+      <tr><th>Price</th><td>{profile.price}</td></tr>
+      <tr><th>Beta</th><td>{profile.beta}</td></tr>
+      <tr><th>Volume Average</th> <td>{profile.volAvg}</td></tr>  
+      <tr><th>Market Cap</th> <td>{profile.mktCap}</td></tr>
+      
     </table>
   );
 }
